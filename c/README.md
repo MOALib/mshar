7bb3a21f8d33edfc1a3de94b0368d636625c22bf
MXPServer 20953 onetechguy <73059135+MXP2095onetechguy@users.noreply.github.com>
2022-05-28 18:12:35 +0700

    Safer code and CMake embedding code upcoming ok

--- c/README.md
+++ c/README.md
@@ -1,3 +1,9 @@
 # MShar
 
 ANSI C Implementation of MShar, under 1k of lines of ANSI C code with realloc and strcat spams.
+
+Link with libm if used with C90 (custom snprintf integer functions) although in reality, you don't need to as we do not use integers.
+
+## CMake Integration
+
+TBA
--- c/src/mshar.h
+++ c/src/mshar.h
@@ -3,7 +3,7 @@
  * @author MXPSQL
  * @brief MShar library in a header.
  * Header only (there is an exception) library for creating MShar Archives, a self extracting archive format based of shar.
- * @version 0 (rolling release)
+ * @version 0 (unscheduled release)
  * @date 2022-05-12
  * 
  * @copyright 
@@ -32,22 +32,28 @@
 
 #ifndef MXPSQL_MShar_H
 /**
- * @brief Include guard
+ * @brief Include guard flag
  * 
  */
 #define MXPSQL_MShar_H
 
 #if defined(__cplusplus) || defined(c_plusplus)
 #include <iostream>
+#include <cmath>
 #include <cstdio>
 #include <cstddef>
 #include <cstdlib>
 #include <cstring>
+#include <cstdarg>
+#include <cstddef>
 #else
+#include <math.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
+#include <stddef.h>
 #endif
 
 #if defined(__cplusplus) || defined(c_plusplus)
@@ -104,15 +110,72 @@ extern "C" {
 char* mkmshar_b64Encode(char *data, size_t inlen);
 
 /**
+ * @brief strnlen function for mkmshar if using C90, uses strlen from string if the standard version is beyond C90.
+ * 
+ * @param str the string to checl
+ * @param maxlen the maximum length of the string
+ * @return size_t the length of the string
+ */
+size_t mkmshar_strnlen(const char *str, size_t maxlen);
+
+/**
+ * @brief Back the int for mkmshar_snprintf if using C90, no use if the standard version is beyond C90.
+ * 
+ * @param x the integer to be converted
+ * @param buf the buffer to be filled
+ * @param size the size of the buffer
+ * @param base the base of the integer
+ * @param uppercase if true, the result will be uppercase
+ * @return int the number of characters written
+ */
+int mkmshar_int_to_str(int x, char *buf, size_t size, int base, int uppercase);
+
+/**
+ * @brief A dumb (and potentially very dummy and gummy) version of vsnprintf that backs mkmshar_snprintf.  
+ * It's quite dumb.
+ * 
+ * Do not use this function for portability reasons (that is that it does not implement everything like '%p').
+ * If if you need more elaborate format flags, go use a C standards compliant version.
+ * This is not a standards compliant version, I just need a dumb one for C90.
+ * Just do not use this even if you don't need elaborate flags like '%p' or '%z'.
+ * 
+ * Derived from https://codereview.stackexchange.com/questions/132860/my-own-snprintf-implementation-in-c
+ * 
+ * @param buf the buffer to be filled
+ * @param size the size of the buffer
+ * @param fmt the format string
+ * @param ap the variadic arguments list
+ * @return int the number of characters written
+ */
+int mkmshar_dumbvsnprintf(char *buf, size_t size, const char *fmt, va_list ap);
+
+/**
+ * @brief snprintf for mkmshar if using C90, uses snprintf from stdio if the standard version beyond C90.
+ * 
+ * mkmshar_dumbvsnprintf is why this function is not portable if the standard version is below C99.
+ * 
+ * @param str the string to copy to
+ * @param size how many bytes to copy
+ * @param format the format string
+ * @param ... the arguments to the format string
+ * @return int the number of bytes copied, negative on failure
+ * 
+ * @see mkmshar_int_to_str
+ * @see mkmshar_strnlen
+ * @see mkmshar_dumbvsnprintf
+ */
+int mkmshar_snprintf(char *str, size_t size, const char *format, ...);
+
+/**
  * @brief Make an MShar archive
  * @warning This function's return value must be manually freed if not null
  * 
- * @param prescript the script to run before extraction, put NULL if empty and do not put the filename, put the content of the script you want to run
- * @param postscript the script to run after extraction, put NULL if empty and do not put the filename, put the content of the script you want to run
+ * @param prescript the script to run before extraction. Put NULL if empty and do not put the filename, put the content of the script you want to run (putting the filename will just place the filename, not the content)
+ * @param postscript the script to run after extraction. Put NULL if empty and do not put the filename, put the content of the script you want to run (putting the filename will just place the filename, not the content)
  * @param files the files to be archived
  * @param nfiles how many files to archive
  * @param ignorefileerrors Ignore file errors and continue, set to 0 to not ignore
- * @return char* the archive or NULL if there is a problem
+ * @return char* the archive or NULL if there is a problem (memory allocation failures, file errors or too big base64)
  */
 char* mkmshar(char* prescript, char* postscript, char** files, size_t nfiles, int ignorefileerrors);
 
@@ -202,9 +265,121 @@ char* mkmshar_b64Encode(char *data, size_t inlen)
     return out;
 }
 
+size_t mkmshar_strnlen(const char *s, size_t maxlen)
+{
+    #if !defined(__STDC_VERSION__) || (__STDC_VERSION__ < 199901L)
+    size_t len = 0;
+    while (len < maxlen && s[len] != '\0') {
+        len++;
+    }
+    return len;
+    #else
+    return strnlen(s, maxlen);
+    #endif
+}
+
+int mkmshar_int_to_str(int x, char *buf, size_t size, int base, int uppercase) {
+    static const int INT_TO_STR_DIGITS_L[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+    static const int INT_TO_STR_DIGITS_U[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+    int length = (int)ceil(log((double)x)/log((double)base));
+    int r, i = 0;
+    char c;
+
+    if (size < ((size_t) length)) {
+        x /= (int)pow(base, (float)(length - size));
+        length = size;
+    }
+
+    do {
+        if (((size_t) i) >= size) break;
+        r = x % base;
+        if (uppercase) {
+            c = INT_TO_STR_DIGITS_U[r];
+        } else {
+            c = INT_TO_STR_DIGITS_L[r];
+        }
+        buf[length-i-1] = c;
+        x /= base;
+        i++;
+    } while (x != 0);
+
+    return i;
+}
+
+int mkmshar_dumbvsnprintf(char *str, size_t size, const char *format, va_list arg_list) {
+    /* dumb snprintf implemenration */
+
+    int chars_printed = 0;
+    char c, *str_arg;
+    int num, len, i;
+    int uppercase = 0, base = 10;
+    size_t max_size = size;
+
+    for (i = 0; format[i] != 0; i++) {
+        if (max_size - chars_printed <= 0) {
+            break;
+        } else if (format[i] == '%') {
+            i++;
+            switch (format[i]) {
+            case 'c':
+                c = va_arg(arg_list, int);
+                str[chars_printed++] = c;
+                break;
+            case '%':
+                str[chars_printed++] = '%';
+                break;
+            case 's':
+                str_arg = va_arg(arg_list, char *);
+                len = mkmshar_strnlen(str_arg, max_size - chars_printed);
+                strncpy(str+chars_printed, str_arg, len);
+                chars_printed += len;
+                break;
+            case 'H':
+                uppercase = 1;
+                continue;
+            case 'h':
+                base = 16;
+                continue;
+            case 'd':
+                num = va_arg(arg_list, int);
+                len = mkmshar_int_to_str(num, str+chars_printed, max_size - chars_printed, base, uppercase);
+                chars_printed += len;
+                break;
+            default:
+                va_end(arg_list);
+                return -1;
+            }
+        } else {
+            str[chars_printed++] = format[i];
+        }
+    }
+
+    if (((size_t) chars_printed) == max_size) chars_printed--;
+    str[chars_printed] = 0;
+
+    return chars_printed;
+}
+
+int mkmshar_snprintf(char *str, size_t size, const char *format, ...)
+{
+    va_list arg_list;
+    int chars_printed = 0;
+    va_start(arg_list, format);
+
+    /* check c standard version */
+    #if !defined(__STDC_VERSION__) || (__STDC_VERSION__ < 199901L)
+    chars_printed = mkmshar_dumbvsnprintf(str, size, format, arg_list);
+    #else
+    chars_printed = vsnprintf(str, size, format, arg_list);
+    #endif
+
+    va_end(arg_list);
+    return chars_printed;
+}
 
 char* mkmshar(char* prescript, char* postscript, char** files, size_t nfiles, int ignorefileerrors){
-    static const size_t initarcfilesize = ((sizeof(char*) + sizeof(size_t)) * 1024);
+    static const size_t initarcfilesize = (((sizeof(char*) + sizeof(size_t)) * 1024) + 1);
     size_t i;
 
     char* arcfile = (char*) MXPSQL_MShar_Calloc(initarcfilesize, initarcfilesize);
@@ -219,16 +394,20 @@ char* mkmshar(char* prescript, char* postscript, char** files, size_t nfiles, in
 "#!/bin/sh \n\
 # This archive is created using MShar, MXPSQL's version of the Shell archiver\n\
 # You need a unix bourne shell and the base64 command to extract this\n\
+\n\
+# shellcheck disable=SC2034 # GNU utilities\n\
+\n\
 TEKTONE=;\n\
+DIRNAME=;\n\
 POSIXLY_CORRECT=1; # Make this posix \n\
 POSIX_ME_HARDER=1; # Make this posix \n\
 TTk=\"$(find /bin /usr/bin /usr/local/bin . -name 'base64*' -type f 2> /dev/null | head -n 1)\";\n\n";
 
         static const char* prestr2 = (char*)
-"printf \"This archive is created with MShar (MXPSQL's version of the Shell archiver)\";\n\
+"printf \"This archive is created with MShar (MXPSQL's version of the Shell archiver)\\n\";\n\
 \n\
 if test -z \"$TTk\"; then\n\
-    printf \"The base64 command is not found you loser. \nIt is needed to extract the archive. \nPlease make it available in PATH or install it.\";\n\
+    printf \"The base64 command is not found you loser. \\nIt is needed to extract the archive. \\nPlease make it available in PATH or install it.\\n\";\n\
     exit 1;\n\
 fi\n\
 \n\n\n";
@@ -365,8 +544,9 @@ fi\n\
                     char* tektsrc = NULL;
 
                     {
-                        char* tektfmt_part1="TEKTONE='";
-                        char* tektfmt_part2="'\n";
+                        static const char* tektfmt_part1=(char*)"TEKTONE='";
+                        static const char* tektfmt_partmid=(char*)"%s";
+                        static const char* tektfmt_part2=(char*)"'\n";
 
                         size_t tektfmt_size = ((strlen(tektfmt_part1) + strlen(files[i]) + strlen(tektfmt_part2)) * sizeof(char*));
 
@@ -381,7 +561,7 @@ fi\n\
                         }
 
                         strcat(tektfmt, tektfmt_part1);
-                        strcat(tektfmt, "%s");
+                        strcat(tektfmt, tektfmt_partmid);
                         strcat(tektfmt, tektfmt_part2);
                     }
 
@@ -411,11 +591,16 @@ fi\n\
                         return NULL;
                     }
                     strcat(fileblock, tektsrc);
+
                     MXPSQL_MShar_Free(tektsrc);
+                    MXPSQL_MShar_Free(tektfmt);
                 }
 
                 {
-                    static char* dirnam = (char*) "mkdir ./$(dirname \"$TEKTONE\") 2> /dev/null;\n";
+                    static char* dirnam = (char*) "\
+DIRNAME=\"./$(dirname \"$TEKTONE\")\"\n\
+mkdir \"$DIRNAME\" 2> /dev/null;\
+\n";
                     if(strlen(dirnam) + strlen(fileblock) > strlen(fileblock)){
                         fileblock = (char*) MXPSQL_MShar_Realloc(fileblock, strlen(dirnam) + strlen(fileblock) + 1);
                         if(fileblock == NULL){
@@ -430,7 +615,7 @@ fi\n\
                 }
 
                 {
-                    static char* info = (char*) "printf \"x - $TEKTONE\";\n";
+                    static char* info = (char*) "printf \"x - %s\\n\" \"$TEKTONE\";\n";
                     if(strlen(info) + strlen(fileblock) > strlen(fileblock)){
                         fileblock = (char*) MXPSQL_MShar_Realloc(fileblock, strlen(info) + strlen(fileblock) + 1);
                         if(fileblock == NULL){
@@ -460,8 +645,10 @@ fi\n\
                 }
 
                 {
-                    char* basprintf = (char*) MXPSQL_MShar_Calloc(initarcfilesize, initarcfilesize);
-                    static const char* fmt = (char*) "printf '%s' > \"./$TEKTONE\";\n";
+                    size_t s = initarcfilesize;
+
+                    char* basprintf = (char*) MXPSQL_MShar_Calloc(s, s);
+                    char* fmt = NULL;
                     char* bas64 = NULL;
                     if(basprintf == NULL){
                         MXPSQL_MShar_Free(arcfile);
@@ -484,12 +671,143 @@ fi\n\
                         strcat(bas64, "");
                     }
 
-                    if(sprintf(basprintf, fmt, bas64) < 0){
+                    {
+                        static const char* fmt1 = (char*) "printf '";
+                        static const char* fmtmid = (char*) "%%s";
+                        static const char* fmt2 = (char*) "' '%s' > \"./$TEKTONE\";\n";
+                        size_t s1 = (sizeof(char*) * strlen(fmt1));
+
+                        char* ss = (char*) MXPSQL_MShar_Calloc(s1, s1);
+                        if(ss == NULL){
+                            MXPSQL_MShar_Free(arcfile);
+                            MXPSQL_MShar_Free(filecontent);
+                            MXPSQL_MShar_Free(fileblock);
+                            MXPSQL_MShar_Free(basprintf);
+                            MXPSQL_MShar_Free(bas64);
+                            if(ss != NULL) MXPSQL_MShar_Free(ss);
+                            return NULL;
+                        }
+
+                        strcat(ss, fmt1);
+
+                        if(strlen(bas64) + s1 > s1){
+                            s1 = strlen(bas64) + s1 + 1;
+                            ss = (char*) MXPSQL_MShar_Realloc(ss, s1);
+                            if(ss == NULL){
+                                MXPSQL_MShar_Free(arcfile);
+                                MXPSQL_MShar_Free(filecontent);
+                                MXPSQL_MShar_Free(fileblock);
+                                MXPSQL_MShar_Free(basprintf);
+                                MXPSQL_MShar_Free(bas64);
+                                if(ss != NULL) MXPSQL_MShar_Free(ss);
+                                return NULL;
+                            }
+                        }
+
+                        strcat(ss, bas64);
+
+                        if(strlen(fmt2) + s1 > s1){
+                            s1 = strlen(fmt2) + s1 + 1;
+                            ss = (char*) MXPSQL_MShar_Realloc(ss, s1);
+                            if(ss == NULL){
+                                MXPSQL_MShar_Free(arcfile);
+                                MXPSQL_MShar_Free(filecontent);
+                                MXPSQL_MShar_Free(fileblock);
+                                MXPSQL_MShar_Free(basprintf);
+                                MXPSQL_MShar_Free(bas64);
+                                if(ss != NULL) MXPSQL_MShar_Free(ss);
+                                return NULL;
+                            }
+                        }
+
+                        strcat(ss, fmt2);
+
+                        MXPSQL_MShar_Free(ss);
+                        ss = NULL;
+
+                        if(s1 > s){
+                            s = s1;
+                            basprintf = (char*) MXPSQL_MShar_Realloc(basprintf, s);
+                            if(basprintf == NULL){
+                                MXPSQL_MShar_Free(arcfile);
+                                MXPSQL_MShar_Free(filecontent);
+                                MXPSQL_MShar_Free(fileblock);
+                                MXPSQL_MShar_Free(bas64);
+                                MXPSQL_MShar_Free(fmt);
+                                if(basprintf != NULL) MXPSQL_MShar_Free(basprintf);
+                                return NULL;
+                            }
+                        }
+
+                        {
+                            size_t fmtlen = strlen(fmt1) + strlen(fmtmid) + strlen(fmt2) + 1;
+                            fmt = (char*) MXPSQL_MShar_Calloc(fmtlen, fmtlen);
+
+                            if(fmt == NULL){
+                                MXPSQL_MShar_Free(arcfile);
+                                MXPSQL_MShar_Free(filecontent);
+                                MXPSQL_MShar_Free(fileblock);
+                                MXPSQL_MShar_Free(basprintf);
+                                MXPSQL_MShar_Free(bas64);
+                                if(fmt != NULL) MXPSQL_MShar_Free(fmt);
+                                return NULL;
+                            }
+
+                            strcat(fmt, fmt1);
+                            strcat(fmt, fmtmid);
+                            strcat(fmt, fmt2);
+                        }
+                    }
+
+                    /* {
+                        size_t sizy;
+                        size_t sizy2;
+                        static const char* fmt1 = (char*) "printf '";
+                        static const char* fmt2 = (char*) "' > \"./$TEKTONE\";\n";
+
+                        bas64 = mkmshar_b64Encode(filecontent, fsize);
+
+                        /\* printf("t: %s", bas64); *\/
+
+                        if(bas64 == NULL){
+                            bas64 = (char*) MXPSQL_MShar_Malloc(sizeof(char));
+                            if(bas64 == NULL){
+                                MXPSQL_MShar_Free(arcfile);
+                                MXPSQL_MShar_Free(filecontent);
+                                MXPSQL_MShar_Free(fileblock);
+                                MXPSQL_MShar_Free(basprintf);
+                                return NULL;
+                            }
+                            strcat(bas64, "");
+                        }
+
+                        sizy = ((strlen(fmt1) + sizeof(bas64) + sizeof(fmt2)) * sizeof(char*));
+                        sizy2 = ((strlen(fmt1) + strlen("%s") + strlen(fmt2)) * sizeof(char*));
+
+                        basprintf = (char*) MXPSQL_MShar_Calloc(sizy, sizy);
+                        fmt = (char*) MXPSQL_MShar_Calloc(sizy2, sizy2);
+
+                        if(basprintf == NULL || fmt == NULL){
+                            MXPSQL_MShar_Free(arcfile);
+                            MXPSQL_MShar_Free(filecontent);
+                            MXPSQL_MShar_Free(fileblock);
+                            if(basprintf != NULL)MXPSQL_MShar_Free(basprintf);
+                            if(fmt != NULL)MXPSQL_MShar_Free(fmt);
+                            return NULL;
+                        }
+
+                        strcat(fmt, fmt1);
+                        strcat(fmt, "%s");
+                        strcat(fmt, fmt2);
+                    } */
+
+                    if(mkmshar_snprintf(basprintf, s, fmt, bas64) < 0){
                         MXPSQL_MShar_Free(arcfile);
                         MXPSQL_MShar_Free(filecontent);
                         MXPSQL_MShar_Free(fileblock);
                         MXPSQL_MShar_Free(basprintf);
                         MXPSQL_MShar_Free(bas64);
+                        MXPSQL_MShar_Free(fmt);
                         return NULL;
                     }
 
@@ -500,6 +818,7 @@ fi\n\
                             MXPSQL_MShar_Free(filecontent);
                             MXPSQL_MShar_Free(fileblock);
                             MXPSQL_MShar_Free(basprintf);
+                            MXPSQL_MShar_Free(bas64);
                             return NULL;
                         }
                     }
@@ -507,6 +826,8 @@ fi\n\
                     strcat(fileblock, basprintf);
 
                     MXPSQL_MShar_Free(bas64);
+                    MXPSQL_MShar_Free(basprintf);
+                    MXPSQL_MShar_Free(fmt);
                 }
 
                 {
